4704f0e14281e72bc64d5b993c6b89fbcb285d24
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2022-10-21 14:15:51 +0300

    Implement allocation stats comparison

--- src/lib.rs
+++ src/lib.rs
@@ -62,12 +62,13 @@ unsafe trait AllocHooks {
 }
 
 use derive_more::Display;
+use serde::{Deserialize, Serialize};
 use std::{
     mem,
     sync::atomic::{AtomicBool, Ordering},
 };
 
-#[derive(Debug, Default, Clone, Display)]
+#[derive(Debug, Default, Clone, Display, Serialize, Deserialize)]
 #[display(fmt = r#"Currently allocated (B): {current}
 Maximum allocated (B): {peak}
 Total amount of claimed memory (B): {total_size}
@@ -169,5 +170,190 @@ unsafe impl AllocHooks for MemoryTracingHooks {
         }
         self.on_dealloc(old_pointer, old_size, align);
         self.on_alloc(new_pointer, new_size, align);
+    }
+}
+
+pub mod cmp {
+
+    use std::fmt::Display;
+
+    use derive_builder::Builder;
+    use num::{rational::Ratio, Integer};
+    use thiserror::Error;
+
+    use crate::MemoryStats;
+
+    #[derive(Debug, Clone, Copy, Default, derive_more::Display)]
+    pub enum Limit<T: Display + Integer + Clone> {
+        #[default]
+        None,
+        #[display(fmt = "{_0}")]
+        Cap(T),
+        #[display(fmt = "{_0}")]
+        Ratio(Ratio<T>),
+    }
+
+    impl<T> Limit<T>
+    where
+        T: Clone + Integer + Display,
+    {
+        pub fn cap(cap: T) -> Self {
+            Limit::Cap(cap)
+        }
+
+        pub fn ratio(numer: T, denom: T) -> Self {
+            Limit::Ratio(Ratio::new(numer, denom))
+        }
+    }
+
+    #[derive(Debug, Error)]
+    #[error("{value} exceeds {ref_value} by {limit}")]
+    pub struct LimitError<T: Display + Integer + Clone> {
+        limit: Limit<T>,
+        value: T,
+        ref_value: T,
+    }
+
+    impl<T> Limit<T>
+    where
+        T: Clone + Integer + Display,
+    {
+        fn check_cap(cap: &T, value: &T, ref_value: &T) -> bool {
+            value.clone() <= ref_value.clone() + cap.clone()
+        }
+
+        fn check_ratio(ratio: &Ratio<T>, value: &T, ref_value: &T) -> bool {
+            value.clone() <= ref_value.clone()
+                || Ratio::new(value.clone() - ref_value.clone(), ref_value.clone()) <= *ratio
+        }
+
+        pub fn check(&self, value: &T, ref_value: &T) -> Result<(), LimitError<T>> {
+            match self {
+                Limit::Cap(cap) if !Self::check_cap(cap, value, ref_value) => Err(LimitError {
+                    limit: self.clone(),
+                    value: value.clone(),
+                    ref_value: ref_value.clone(),
+                }),
+                Limit::Ratio(ratio) if !Self::check_ratio(ratio, value, ref_value) => {
+                    Err(LimitError {
+                        limit: self.clone(),
+                        value: value.clone(),
+                        ref_value: ref_value.clone(),
+                    })
+                }
+                _ => Ok(()),
+            }
+        }
+    }
+
+    /// Limits for each allocation statistics parameter.
+    #[derive(Debug, Builder)]
+    pub struct AllocLimits {
+        #[builder(default)]
+        pub current: Limit<usize>,
+        #[builder(default)]
+        pub peak: Limit<usize>,
+        #[builder(default)]
+        pub total_size: Limit<usize>,
+        #[builder(default)]
+        pub total_num: Limit<usize>,
+        #[builder(default)]
+        pub reallocs: Limit<usize>,
+    }
+
+    #[derive(Debug, Error)]
+    #[error("Allocation parameter `{param}`: {error}")]
+    pub struct AllocLimitsError {
+        error: LimitError<usize>,
+        param: &'static str,
+    }
+
+    macro_rules! check {
+        ($f:ident, $l:expr, $v:expr, $r:expr) => {
+            $l.$f.check(&$v.$f, &$r.$f).map_err(|e| AllocLimitsError {
+                error: e,
+                param: stringify!($f),
+            })
+        };
+    }
+
+    impl AllocLimits {
+        pub fn check(
+            &self,
+            value: &MemoryStats,
+            ref_value: &MemoryStats,
+        ) -> Result<(), AllocLimitsError> {
+            check!(current, self, value, ref_value)?;
+            check!(peak, self, value, ref_value)?;
+            check!(total_size, self, value, ref_value)?;
+            check!(total_num, self, value, ref_value)?;
+            check!(reallocs, self, value, ref_value)?;
+            Ok(())
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn limit_cap() {
+            let l = Limit::cap(10_u32);
+            let r = 100_u32;
+            assert!(l.check(&0, &r).is_ok());
+            assert!(l.check(&100, &r).is_ok());
+            assert!(l.check(&110, &r).is_ok());
+            assert!(l.check(&111, &r).is_err());
+
+            println!("{}", l.check(&111, &r).unwrap_err());
+        }
+
+        #[test]
+        fn limit_ratio() {
+            let l = Limit::ratio(1, 10);
+            let r = 100_u32;
+            assert!(l.check(&0, &r).is_ok());
+            assert!(l.check(&100, &r).is_ok());
+            assert!(l.check(&110, &r).is_ok());
+            assert!(l.check(&111, &r).is_err());
+
+            println!("{}", l.check(&111, &r).unwrap_err());
+        }
+
+        #[test]
+        fn limits() {
+            let rs = MemoryStats {
+                current: 100,
+                peak: 1000,
+                total_size: 2000,
+                total_num: 100,
+                reallocs: 0,
+            };
+            let vs = MemoryStats {
+                current: 110,
+                peak: 1100,
+                total_size: 2200,
+                total_num: 110,
+                reallocs: 1,
+            };
+
+            let ls = AllocLimitsBuilder::default().build().unwrap();
+            assert!(ls.check(&rs, &rs).is_ok());
+            assert!(ls.check(&vs, &rs).is_ok());
+
+            let ls = AllocLimitsBuilder::default()
+                .current(Limit::Cap(1))
+                .build()
+                .unwrap();
+            let r = ls.check(&vs, &rs);
+            assert!(r.unwrap_err().param == "current");
+
+            let ls = AllocLimitsBuilder::default()
+                .reallocs(Limit::Cap(0))
+                .build()
+                .unwrap();
+            let r = ls.check(&vs, &rs);
+            assert!(r.unwrap_err().param == "reallocs");
+        }
     }
 }
